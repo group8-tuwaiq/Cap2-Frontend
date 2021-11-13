269e192d6c753f5026b29244ad842cb1c1ada1b5
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-13 18:13:33 +0300

    slider

---
+++ slider/slid/src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Carousel,Nav,Navbar } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+      <Carousel>
+      <Carousel.Item>
+
+<img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
+
+        </Carousel.Item>
+        
+        <Carousel.Item>
+
+<img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
+
+        </Carousel.Item>
+
+        <Carousel.Item>
+
+<img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
+
+        </Carousel.Item>
+
+        <Carousel.Item>
+
+<img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
+
+        </Carousel.Item>
+
+        <Carousel.Item>
+
+<img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
+
+        </Carousel.Item>
+
+
+        <Carousel.Item>
+
+<img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
+
+        </Carousel.Item>
+        
+        
+        
+        
+        
+        </Carousel> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
