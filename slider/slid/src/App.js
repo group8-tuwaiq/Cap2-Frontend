85eac296afa7955d10f818623f68546c9a16e772
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-13 19:51:02 +0300

    slider

--- slider/slid/src/App.js
+++ slider/slid/src/App.js
@@ -14,30 +14,66 @@ function App() {
 
 <img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
 
+<Carousel.Caption>
+
+<h2>Text here</h2>
+<p>here description</p>
+
+</Carousel.Caption>
+       
         </Carousel.Item>
         
         <Carousel.Item>
 
 <img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
 
+        
+<Carousel.Caption>
+
+<h2>Text here</h2>
+<p>here description</p>
+
+</Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
 
 <img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
 
+<Carousel.Caption>
+
+<h2>Text here</h2>
+<p>here description</p>
+
+</Carousel.Caption>
+       
         </Carousel.Item>
 
         <Carousel.Item>
 
 <img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
 
+<Carousel.Caption>
+
+<h2>Text here</h2>
+<p>here description</p>
+
+</Carousel.Caption>
+        
         </Carousel.Item>
 
         <Carousel.Item>
 
 <img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
 
+<Carousel.Caption>
+
+<h2>Text here</h2>
+<p>here description</p>
+
+</Carousel.Caption>
+       
+       
         </Carousel.Item>
 
 
@@ -45,6 +81,14 @@ function App() {
 
 <img src="https://www.planetbroadcasting.com/wp-content/uploads/2020/05/Annotation-2020-05-04-081104-1.jpg" height="600px" width="900px" alt="img"/>
 
+<Carousel.Caption>
+
+<h2>Text here</h2>
+<p>here description</p>
+
+</Carousel.Caption>
+        
+        
         </Carousel.Item>
         
         
