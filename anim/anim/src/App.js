d94c2fb9253f301b2b649aaaf9a2c025f6ce0080
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-14 15:07:51 +0300

    done

--- anim/anim/src/App.js
+++ anim/anim/src/App.js
@@ -20,8 +20,8 @@ function App() {
 <Card.Text style={{color:"black"}}>
  Movies Description
 </Card.Text>
-<Button className="anim" style={{margin: "13px"}} variant="danger">More</Button>
- <Button className="anim" variant='light'>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
+<Button className="btn" style={{margin: "13px"}} variant="danger">More</Button>
+ <Button className="btn" variant='light'>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
 </svg> </Button>
 </Card.Body>
@@ -38,8 +38,8 @@ function App() {
 <Card.Text style={{color:"red"}}>
 Movies Description
 </Card.Text>
-<Button className="anim" style={{margin: "13px"}}  variant="danger">More</Button>
- <Button className="anim" variant='light'>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
+<Button className="btn" style={{margin: "13px"}}  variant="danger">More</Button>
+ <Button className="btn" variant='light'>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
 </svg> </Button>
 </Card.Body>
@@ -54,8 +54,8 @@ Movies Description
 <Card.Text style={{color:"red"}}>
  Movies Description
 </Card.Text>
-<Button className="anim" style={{margin: "13px"}}  variant="danger">More</Button>
- <Button className="anim" variant='light'>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
+<Button className="btn" style={{margin: "13px"}}  variant="danger">More</Button>
+ <Button className="btn" variant='light'>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
 </svg> </Button>
 </Card.Body>
@@ -63,23 +63,7 @@ Movies Description
 <small className="text-muted">Watch Movie Now</small>
 </Card.Footer>
 </Card>
-<Card className="anim" style={{ color:"red"},{margin: "13px"}}>
-<Card.Img className="anim" variant="top" src="https://i.imgflip.com/1yj1os.gif" height="350" width="250" alt="no image"  />
-<Card.Body>
-<Card.Title  style={{color:"red"}}>Movies Name</Card.Title>
-<Card.Text  style={{color:"red"}}>
- Movies Description
-</Card.Text>
-<Button className="anim" variant="danger">More</Button>
- <Button className="anim" variant='light'>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
-<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-</svg> </Button>
-</Card.Body>
-<Card.Footer>
-<small className="text-muted">Watch Movie Now</small>
-</Card.Footer>
 
-</Card> 
 
 </CardGroup>
 
